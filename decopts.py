ec45273aefcc66294273e5e3799bf5e057709eaf
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2017-10-29 18:46:05 -0600

    initial commit

---
+++ decopts.py
@@ -0,0 +1,102 @@
+from __future__ import absolute_import
+from argparse import ArgumentParser
+from collections import deque
+
+
+def to_run(func, wrapper):
+    func = get_parent(func)
+    if not hasattr(func, '_run'):
+        func._run = deque()
+
+    func._run.appendleft(wrapper)
+
+
+def do_run(func):
+    if hasattr(func, '_run'):
+        for wrapper_func in func._run:
+            wrapper_func()
+
+        delattr(func, '_run')
+
+
+def get_parent(func):
+    while hasattr(func, 'parent'):
+        func = func.parent
+
+    return func
+
+
+def get_func(func):
+    while hasattr(func, 'func'):
+        func = func.func
+
+    return func
+
+
+def entrypoint(*args, **kwds):
+    def wrapper(func):
+        def run(*args, **kwds):
+            do_run(func)
+            for action in func.actions.keys():
+                do_run(func.actions[action])
+
+            func.args = func.parser.parse_args()
+            for action in func.actions.keys():
+                func.actions[action].args = func.args
+
+            func(*args, **kwds)
+            if func.args.action in func.actions:
+                func.actions[func.args.action]()
+
+        func = get_func(func)
+        func.parser = ArgumentParser(*args, **kwds)
+        if not hasattr(func, 'actions'):
+            func.actions = {}
+
+        run.func = func
+        return run
+
+    return wrapper
+
+
+def defaults(*args, **kwds):
+    def wrapper(func):
+        func = get_func(func)
+        to_run(func, lambda: func.parser.set_defaults(*args, **kwds))
+        return func
+
+    return wrapper
+
+
+def option(*args, **kwds):
+    def wrapper(func):
+        func = get_func(func)
+        to_run(func, lambda: func.parser.add_argument(*args, **kwds))
+        return func
+
+    return wrapper
+
+
+def action(parent, name, *args, **kwds):
+    def wrapper(func):
+        def action_wrapper():
+            if not hasattr(parent, 'subparser'):
+                parent.subparser = parent.parser.add_subparsers()
+
+            func.parser = parent.subparser.add_parser(name, *args, **kwds)
+            if not hasattr(func, 'actions'):
+                func.actions = {}
+
+            defaults(func, action=name)
+
+        func = get_func(func)
+        func.parent = parent
+        if not hasattr(parent, 'actions'):
+            parent.actions = {}
+
+        parent.actions[name] = func
+        to_run(func, action_wrapper)
+        return func
+
+    parent = get_func(parent)
+    return wrapper
---
+++ setup.py
@@ -0,0 +1,101 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+import pypandoc
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), 'w') as f:
+    long_description = pypandoc.convert_file('README.md', 'rst')
+    f.write(long_description + '\n')
+
+setup(
+    name='decopts',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.0.0',
+
+    description='Use decorators for dealing with argparse',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/Eeems/intercessions',
+
+    # Author details
+    author='Nathaniel van Diepen',
+    author_email='eeems13@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        # Development Status :: 1 - Planning
+        # Development Status :: 2 - Pre-Alpha
+        # Development Status :: 3 - Alpha
+        # Development Status :: 4 - Beta
+        # Development Status :: 5 - Production/Stable
+        # Development Status :: 6 - Mature
+        # Development Status :: 7 - Inactive
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+
+        # Topics
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Terminals',
+    ],
+
+    # What does your project relate to?
+    keywords='optparse argparse',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['pypandoc']
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={},
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={},
+)
